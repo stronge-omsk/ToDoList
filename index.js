012dce1d79c67456b0c235181f11d3d1b166fd27
Alexandr <silnii.omsk@gmail.com>
2020-01-25 03:07:12 +0600

    added a text input field, a button and deleting created divs

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log(1)
+document.getElementById('Btn').onclick = function() {
+  const inp = document.getElementById('text');
+  const type = inp.value;
+  inp.value = '';
+  const list = document.createElement('div');
+  list.innerText = type;
+  document.getElementsByClassName('main')[0].appendChild(list);
+ 
+  const press = document.createElement('input');
+  press.type = 'button';
+  press.value = 'Delete';
+  list.appendChild(press);
+
+
+}
