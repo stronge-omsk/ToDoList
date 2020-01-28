da86219605db1dad3e40403a473054f0f947cf8f
Alexandr <silnii.omsk@gmail.com>
2020-01-29 02:33:14 +0600

    added changes to css

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ document.getElementById('Btn').onclick = function () {
   inp.value = '';
   const list = document.createElement('div');
   list.innerText = type;
-  // list.classList.add('younger-div');
+  list.classList.add('todo-item');
   document.getElementsByClassName('todo-list')[0].appendChild(list);
   const press = document.createElement('input');
   press.type = 'button';
   press.value = 'Delete';
+  press.classList.add('todo-button-delete');
   list.appendChild(press);
   press.addEventListener("click", (event) => {
     list.remove('div')
