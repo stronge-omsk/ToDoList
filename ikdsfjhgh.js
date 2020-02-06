ed9bc7b3cc92e9bdb29510192c62378a9035e32f
Alexandr <silnii.omsk@gmail.com>
2020-02-06 17:29:57 +0600

    rewrote on jquery

---
+++ ikdsfjhgh.js
@@ -0,0 +1,22 @@
+document.getElementById('Btn').onclick = function () {
+  const inp = document.getElementById('text');
+  const type = inp.value;
+  inp.value = '';
+  if (type !== "") {
+    const list = document.createElement('div');
+    list.innerText = type;
+    list.classList.add('todo-item');
+    document.getElementsByClassName('todo-list')[0].appendChild(list);
+    const press = document.createElement('input');
+    press.type = 'button';
+    press.value = 'Delete';
+    press.classList.add('todo-button-delete');
+    list.appendChild(press);
+    press.addEventListener("click", (event) => {
+      list.remove('div')
+
+    });
+  }
+
+
+};
--- index.js
+++ index.js
@@ -1,28 +1,140 @@
-document.getElementById('Btn').onclick = function () {
-  const inp = document.getElementById('text');
-  const type = inp.value;
-  inp.value = '';
-  if (type !== "") {
+const todoList = [];
+$('#Btn').on('click', addItem)
 
-
-    const list = document.createElement('div');
-    list.innerText = type;
-    list.classList.add('todo-item');
-    document.getElementsByClassName('todo-list')[0].appendChild(list);
-    const press = document.createElement('input');
-    press.type = 'button';
-    press.value = 'Delete';
-    press.classList.add('todo-button-delete');
-    list.appendChild(press);
-    press.addEventListener("click", (event) => {
-      list.remove('div')
-
-    });
+function addItem() {
+  const todoInput = $('#text')[0];
+  if (!todoInput.value) {
+    return;
   }
-
-
-
-
+  todoList.push(todoInput.value)
+  todoInput.value = '';
+  renderToDoList();
 
 };
+// document.getElementById('Btn').addEventListener('click', addItem)
+// const b = document.createElement('button')
+// document.body.appendChild(b)
+// console.log(b);
+// $('#Btn').on('click', addItem)
 
+function renderToDoList() {
+  const list = $('.todo-list')[0];
+
+  while (list.firstChild) {
+    list.removeChild(list.firstChild)
+  }
+  for (let i = 0; i < todoList.length; i++) {
+    const listItem = $ ().text(todoList[i]); //document.createTextNode(todoList[i]);
+    const li = $('<li>') // const li = document.createElement('li');
+    li.text(todoList[i])
+    //   li.appendChild(document.createTextNode(todoList[i]));
+    const deleteBtn = $('<input>');
+    deleteBtn.attr('type', 'button');
+    deleteBtn.val('Delete');   //= 'Delete';
+    $(deleteBtn).on('click', function () {
+      deleteItem(i);
+    })
+    li.append(deleteBtn);
+    li.addClass('todo-item');
+    list.append(li);
+
+  }
+
+}
+
+function deleteItem(index) {
+  todoList.splice(index, 1)
+
+
+  renderToDoList();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let i = 0;
+// while (i < 10) {
+//   i++;
+// }
+
+
+// const todoList = [];
+// document.getElementById('Btn').addEventListener('click', function () {
+//   const list = document.getElementsByClassName('todo-list')[0];
+//   list.value = '';
+//   const todoInput = document.getElementById('text').value;
+//   todoList.push(todoInput)
+
+//   const item = document.createElement('li');
+//    todoList.forEach (function() {
+//     const listItem = document.createTextNode(todoList[i]);
+//     const li = document.createElement('li');
+//     li.appendChild(document.createTextNode(todoList[i]));
+//     list.append(li);
+//   })
+// });
+
+
+
+
+
+// // const todoList = [];
+// document.getElementById('Btn').onclick = function () {
+//   const inp = document.getElementById('text');
+//   const type = inp.value;
+//   inp.value = '';
+//   const todoList = [1];
+//   todoList.forEach(myFunction);
+//   function myFunction() {
+//     const list = document.createElement('div');
+//     list.innerText = type;
+//     list.classList.add('todo-item');
+//     document.getElementsByClassName('todo-list')[0].appendChild(list);
+//     const press = document.createElement('input');
+//     press.type = 'button';
+//     press.value = 'Delete';
+//     press.classList.add('todo-button-delete');
+//     list.appendChild(press);
+//     press.addEventListener("click", (event) => {
+//       list.remove('div')
+
+//     });
+//   }
+
+
+// };
